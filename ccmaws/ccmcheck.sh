1875e851db33ec476c49af781dbe4502f1210ef9
rangapv <rangapv@yahoo.com>
2021-12-01 11:25:17 +0000

    Created secrets in kube-ssytem

--- ccmaws/ccmcheck.sh
+++ ccmaws/ccmcheck.sh
@@ -20,16 +20,26 @@ filesecret() {
 kfs=`kubectl create secret generic aws-secret --from-file=aws.conf=/etc/kubernetes/aws.conf --from-file=config=$HOME/.kube/config --from-file=AWS_ACCESS_KEY_ID=$HOME/access_id --from-file=AWS_SECRET_ACCESS_KEY=$HOME/secret_id -n kube-system`
 skfs="$?"
 
-if [[ $skfs -eq 0 ]]
-then
-	echo "Successfully created aws-secret"
-else
-	echo "aws-secret creation failed"
-fi
-
+kfsac1=`kubectl create secret generic aws-access-1 --from-file=AWS_ACCESS_KEY_ID=$HOME/access_id -n kube-system`
+skfsac1s="$?"
+ssmstat aws-access-1 $skfsac1s
+kfsac2=`kubectl create secret generic aws-access-2 --from-file=AWS_SECRET_ACCESS_KEY=$HOME/secret_id -n kube-system`
+skfsac2s="$?"
+ssmstat aws-access-2 $skfsac2s
 
 }
 
+ssmstat() {
+
+ssmstat1="$@"
+
+if [[ $2 -eq 0 ]]
+then
+	echo "$1 create successful"
+else
+	echo "$1 create failed"
+fi
+}
 
 filecheck /etc/kubernetes/aws.conf
 filecheck $HOME/.kube/config
